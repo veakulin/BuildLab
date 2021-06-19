72ab82c25eee55570833706ee75f4d796a799a7f
veakulin <vea@live.ru>
2021-06-19 17:08:19 +0400

    Initial commit

---
+++ CSApp/CSApp.cs
@@ -0,0 +1,9 @@
+﻿using ru.akulin.BuildLab.CSLib;
+
+namespace ru.akulin.BuildLab.CSApp
+{
+    class CSApp
+    {
+        static void Main() => Utils.DoWork();
+    }
+}
---
+++ CSLib/Utils.cs
@@ -0,0 +1,7 @@
+﻿namespace ru.akulin.BuildLab.CSLib
+{
+    public static class Utils
+    {
+        public static  void DoWork() { }
+    }
+}
